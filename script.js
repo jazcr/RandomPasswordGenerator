0d8be141d3e62ca5e781ac171fd268c7eea4c1c3
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-09 14:33:52 -0600

    update script

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 
 var generateBtn = document.querySelector("#generate");
-var ranSymbol = ["!@#$%^&*_+=?><?"];
+var passLength;
+var getLowerLtr;
+var getUpperLtr;
+var getRanNum;
+var getSpecChar = ["!@#$%^&*_+=?><?"];
+var userChoices;
 
+
+
+// //Generating lowercase letters
+function getLowerLtr() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  getLowerLtr = lower;
+}
+
+
+//Generating upercase letters
+function getUpperLtr() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  getUpperLtr = upper;
+}
+
+
+//Generating random numbers
+
+function getRanNum() {
+	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  getRanNum = num;
+}
+
+
+//Generating randomsymbol
+function getSpecChar() {
+	const symbols = '!@#$%^&*_+=?<>'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+  getSpecChar = symbols;
+}
 
 
 
@@ -18,10 +53,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-get.addEventListener("click", function () {
-    pw = generatePassword();
-    document.querySelector("#password").placeholder = ps;
-});
+// get.addEventListener("click", () => {
+//     pw = generatePassword();
+//     document.querySelector("#password").placeholder = pw;
+// });
 
 // Start function to generate password
 function generatePassword() {
@@ -30,10 +65,27 @@ function generatePassword() {
     if (!passLength) {
         alert ("Please enter value between 8 and 128.")
     } else if (passLength < 8 || passLength > 128) {
-        passLength = parseInt(prompt("Please enter value between 8 and 128."))
-    } else {
-        
+        passLength = alert("Invalid input. Please enter a value between 8 and 128.")
+    }
+      //Prompting user for other password criteria
+      else {
+       getLowerLtr = confirm("Press 'OK' if you'd like to include LOWERCASE letters. If not, press 'CANCEL'.");
+       getUpperLtr = confirm("Press 'OK' if you'd like  to include UPPERCASE letters. If not, press 'CANCEL'.");
+       getRanNum = confirm("Press 'OK' if you'd like to include NUMBERS. If not, press 'CANCEL'.");
+       getSpecChar = confirm("Press 'OK' if you'd like to include SPECIAL CHARACTERS. If not, press 'CANCEL'.");
     }
 
-}
+};
+
+
+
+//Alert if no criteria are chosen
+if ( passLength && !getLowerLtr && !getUpperLtr && !getRanNum && !getSpecChar ) {
+  userChoices = alert("At least one criteria must be chosen. Please start over.");
+
+} 
+
+// if (getLowerLtr && getUpperLtr && getRanNum && getSpecChar){
+//    getLowerLtr.concat(getUpperLtr, getRanNum, getSpecChar);
+//passlLength $$ }
 
